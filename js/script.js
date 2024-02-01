b14231f8620e597415a79f084fcbeef87f7d1371
Skyesblue <210711019@students.uajy.ac.id>
2024-02-01 21:32:37 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slider");
+        let dots = document.getElementsByClassName("dot");
+        for(i = 0; i < slides.length; i++){
+            slides[i].classList.add("hidden");
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+
+        for(i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex-1].classList.remove("hidden");
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2000);
+    }
+});
